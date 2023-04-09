c6e9dcfe1915c062d7105e9bc5a069a689155d60
Patrick Short <pshort0905@gmail.com>
2023-04-09 09:40:19 -0400

    Adds code to index.js for test.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat() {
+    return [...cats, "Broom"]
+}
+
+function prependCat() {
+    return ["Arnold", ...cats]
+}
+
+function removeLastCat() {
+   return cats.slice(0,-1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
